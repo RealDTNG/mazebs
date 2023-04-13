8584d5ebbad9bae7e6dac4cc59d63ace3bda86a6
RealDTNG <ddwh2005@gmail.com>
2023-04-13 12:44:38 -0400

    done

--- bleh.py
+++ bleh.py
@@ -1,66 +1,50 @@
 # Dawson Hoyle
 # March 31 2023
 # Basic PyGame Setup Code
+
 import pygame,sys
 from maze_img import Maze
 from player import thing
 
 
-pygame.init()
+input(f"Welcome to the maze game.\nYour goal is to travel to the glowing square. Once you reach the glowing square the program will close and you will win.\n\nPress [Enter] to start")
+
 # Game Setup
-fps = 30
+pygame.init()
+fps = 60
 fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
 
+
 #Setup of Starting objects
-
-
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
-
 maze_map = pygame.image.load('maze_map.png')
 map = Maze(0, 0, 500, 500, maze_map)
 map_g = pygame.sprite.Group()
 map_g.add(map)
-
 player = thing(220, 220, 15,(82, 242, 114))
 player_g = pygame.sprite.Group()
 player_g.add(player)
-
 pygame.display.set_caption("God make this my last maze game")
 key_input = ""
 red = pygame.color.Color(255,0,0)
 green = pygame.color.Color(0,255,0)
 blue= pygame.color.Color(0,0,255)
+reccolor = "red"
 
-
-colourstr = "red"
-reccolor = red
-
-def changecolour():
-    if colourstr == "green":
-        colourstr = "blue"
-        reccolor = blue
-        return
-    if colourstr == "blue":
-        colourstr = "red"
-        reccolor = red
-        return
-    if colourstr == "red":
-        colourstr = "green"
-        reccolor = green
-        return
 
 def display():
-    global winrec
+    global winrec,reccolor
+    colourdic = {"red":red,"green":green,"blue":blue}
+    colourchange = {"red":"green","green":"blue","blue":"red"}
     window.fill((255,255,255)) #White background
     map_g.draw(window)
-    winrec =pygame.draw.rect(window,(reccolor),(293,10,72,72))
-    changecolour()
+    winrec = pygame.draw.rect(window,(colourdic[reccolor]),(293,10,72,72))
+    reccolor = colourchange[reccolor]
     player_g.draw(window)
     
-        
-display()
+
 while True:
     player.move()
     display()
@@ -70,14 +54,12 @@ while True:
     if winrec.colliderect(player):
             pygame.quit()
             sys.exit()
-    
-        
     for event in pygame.event.get():
     # if user  QUIT then the screen will close
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
   
-       
+
     pygame.display.update() #update the display
     fpsClock.tick(fps) #speed of redraw
